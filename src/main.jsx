290f33ba3e1fb512ee67898e38991306f7e0fd10
shekar <null>
2024-10-08 12:07:26 +0530

    git-pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 createRoot(document.getElementById("root")).render(
